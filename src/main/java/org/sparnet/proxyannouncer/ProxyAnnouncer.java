82173ded07b850a0408298ace6ad084426ca8504
sparnet <alessandropanait69@gmail.com>
2024-05-14 16:08:14 +0200

    1.0 global

--- src/main/java/org/sparnet/proxyannouncer/ProxyAnnouncer.java
+++ src/main/java/org/sparnet/proxyannouncer/ProxyAnnouncer.java
@@ -1,16 +1,43 @@
 package org.sparnet.proxyannouncer;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
 
-public final class ProxyAnnouncer extends Plugin {
+public class ProxyAnnouncer extends Plugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Plugin abilitato!");
+        // Registra il comando
+        getProxy().getPluginManager().registerCommand(this, new AnnuncioCommand());
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    private class AnnuncioCommand extends Command {
+
+        public AnnuncioCommand() {
+            super("annuncio", "admin.annuncio");
+        }
+
+        @Override
+        public void execute(CommandSender sender, String[] args) {
+            if (args.length == 0) {
+                sender.sendMessage("Utilizzo: /annuncio <messaggio>");
+                return;
+            }
+
+            StringBuilder message = new StringBuilder();
+            for (String word : args) {
+                message.append(word).append(" ");
+            }
+
+            // Aggiungi il prefix "[Annuncio]" al messaggio
+            String fullMessage = "[Annuncio] " + ChatColor.RED + message.toString();
+
+            // Invia il messaggio a tutti i server collegati
+            ProxyServer.getInstance().broadcast(fullMessage);
+        }
     }
 }
